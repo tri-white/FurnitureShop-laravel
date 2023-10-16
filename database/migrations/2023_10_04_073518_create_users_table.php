8dae6c0936b526d30e70014d689faea19c67d480
tri-white <olegovic101@gmail.com>
2023-10-16 22:31:59 +0300

    tested migrations

--- database/migrations/2023_10_04_073518_create_users_table.php
+++ database/migrations/2023_10_04_073518_create_users_table.php
@@ -13,9 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('login');
+            $table->string('username');
             $table->string('password');
-            $table->string('photo')->nullable();
             $table->boolean('admin')->default(false);
             $table->timestamps();
         });
