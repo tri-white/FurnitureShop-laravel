96ce0a7b39c29ee4ed6cd3dbd9263a4afb053da3
tri-white <olegovic101@gmail.com>
2023-10-24 20:16:50 +0300

    added delete() routes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::post('/registration', [UserController::class, 'registration'])->name('reg
 Route::get('/login', [UserController::class, 'loginView'])->name('loginView');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/delete-user/{userId}', [UserController::class, 'delete'])->name('delete-user');
 
 Route::get('/shop/page={page}/searchKey={searchKey}/category={category}/sort={sort}', [ShopController::class, 'index'])->name('shop');
 Route::post('/search', [ShopController::class, 'search'])->name('search');
@@ -30,8 +31,10 @@ Route::get('/add-product', [ShopController::class, 'addView'])->name('add-produc
 
 Route::post('/add-product', [ProductController::class, 'add'])->name('add-product');
 Route::get('/product/{id}', [ProductController::class, 'details'])->name('product-details');
+Route::get('/delete-product/{productId}', [ProductController::class, 'delete'])->name('delete-product');
 
 Route::post('/comment/{userid}/{productid}', [CommentController::class, 'add'])->name('add-comment');
+Route::post('/delete-comment/{commentId}', [CommentController::class, 'delete'])->name('delete-comment');
 
 Route::get('/wishlist/{userid}', [WishController::class, 'index'])->name('wishlist');
 Route::get('/wishlist/add/{userid}/{productid}', [WishController::class, 'add'])->name('add-wish');
@@ -46,6 +49,7 @@ Route::post('/update-cart/{userid}/{productid}', [CartController::class, 'update
 Route::get('/all-orders', [OrderController::class, 'allOrders'])->name('all-orders');
 Route::post('/place-order/{userid}', [OrderController::class, 'placeOrder'])->name('place-order');
 Route::get('/order/{orderid}', [OrderController::class, 'order'])->name('order');
+Route::get('/delete-order/{orderId}', [OrderController::class, 'delete'])->name('delete-order');
 
 
 
