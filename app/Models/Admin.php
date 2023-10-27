911f6965252ceb5f9a4691d7165c0a401ac33212
tri-white <olegovic101@gmail.com>
2023-10-27 18:21:40 +0300

    added tokens for admins

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Laravel\Sanctum\HasApiTokens;
 class Admin extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasApiTokens;
     protected $guard = 'admin';
 
     protected $fillable = [
